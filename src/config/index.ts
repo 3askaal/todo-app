2bf9ae3b0337342d4f0a34e340c07690d58d925b
Bas Kaal <3askaal@gmail.com>
2020-04-06 19:51:57 +0200

    progress

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,4 @@
 export const API_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://t0d0-4p1.herokuapp.com'
+    ? 'https://todo-4pi.herokuapp.com'
     : 'http://localhost:4000'
-export const BASE_URL = process.env.NODE_ENV === 'production' ? '/todo-app' : ''
